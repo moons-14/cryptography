6c79edff4ad908d34e322a81588dcea49c20e012
moons14 <moons.music.mail@gmail.com>
2021-01-01 20:32:49 +0900

    Update cryptography.php

--- cryptography.php
+++ cryptography.php
@@ -1,11 +1,7 @@
 <?php
-/**
-
-Copyright (c) 2021 Moons Softwares
-
-This software is released under the MIT License.
-http://opensource.org/licenses/mit-license.php
-*/
+// Copyright (c) 2021 Moons Softwares
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
 header('Content-Type: application/json; charset=UTF-8');
 if($_GET['mode']=="encryption"){
     if(isset($_GET['key'])&&isset($_GET['text'])){
